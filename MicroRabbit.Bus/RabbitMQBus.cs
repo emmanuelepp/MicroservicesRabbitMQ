52a4e2db6fa03571870364bc86f6532e3e3d6e47
Emmanuel Ponciano <emmanuelponciano@gmail.com>
2022-07-22 21:39:55 -0400

    Add publish logic and rabbit-mq settings

---
+++ MicroRabbit.Bus/RabbitMQBus.cs
@@ -0,0 +1,62 @@
+﻿using MediatR;
+using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Domain.Core.Commands;
+using MicroRabbit.Domain.Core.Events;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace MicroRabbit.Bus
+{
+    public sealed class RabbitMQBus : IEventBus
+    {
+        private readonly IMediator _mediator;
+        private readonly Dictionary<string, List<Type>> _handlers;
+        private readonly List<Type> _eventTypes;
+        private readonly RabbitMQSettings _settings;
+
+        public RabbitMQBus( IMediator mediator, IOptions<RabbitMQSettings> settings)
+        {
+            _mediator = mediator;
+            _handlers = new Dictionary<string, List<Type>>();
+            _eventTypes = new List<Type>();
+            _settings = settings.Value;
+        }
+        public void Publish<T>(T @event) where T : Event
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = _settings.HostName,
+                UserName = _settings.UserName,
+                Password = _settings.Password
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var eventName = @event.GetType().Name;
+
+                channel.QueueDeclare(eventName, false, false, false, null);
+
+                var message = JsonConvert.SerializeObject(@event);
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("", eventName, null, body);
+            }
+        }
+
+        public Task SendCommand<T>(T command) where T : Command
+        {
+           return _mediator.Send(command);
+        }
+
+        public void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
