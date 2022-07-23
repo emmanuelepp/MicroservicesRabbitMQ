b75828494fcfad136946530f1866f2e37536897d
Emmanuel Ponciano <emmanuelponciano@gmail.com>
2022-07-22 20:57:59 -0400

    Create MRMQ
    Add new project.

---
+++ MicroRabbit.Domain.Core/Events/Event.cs
@@ -0,0 +1,13 @@
+﻿namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Event
+    {
+        public string MessageType { get; protected set; }
+
+        protected Event()
+        {
+            MessageType = GetType().Name;
+        }
+    }
+}
+
